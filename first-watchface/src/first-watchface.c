4bf32709f7ff0e2d1ccf53205acab8e0bb6449f9
Zach <zstecker@princeton.edu>
2016-07-09 12:55:26 -0400

    add date and color

--- first-watchface/src/first-watchface.c
+++ first-watchface/src/first-watchface.c
@@ -2,26 +2,48 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 
 static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void update_date() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), 
-          clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), "%b %e", tick_time);
 
-  text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_date_layer, s_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  if ((units_changed & MINUTE_UNIT) != 0) {
+    update_time();
+  }
+
+  if ((units_changed & DAY_UNIT) != 0) {
+    update_date();
+  }
 }
 
 static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // TIME
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
@@ -31,28 +53,49 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  // DATE
+  s_date_layer = text_layer_create(GRect(0, 105, bounds.size.w, 30));
+
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
 }
 
+
 static void init() {
-  // init window, set handlers, push to stack
+  // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
-  window_set_background_color(s_main_window, GColorYellow);
+
+  window_set_background_color(s_main_window, GColorSunsetOrange);
+
+  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 
+  // Make sure the time is displayed from the start
   update_time();
+  update_date();
 
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT | DAY_UNIT, tick_handler);
 }
 
 static void deinit() {
+  // Destroy Window
   window_destroy(s_main_window);
 }
 
